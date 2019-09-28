8be9376451381a97a976192d84e80de3d17d08b7
spylypets <sergiy1@yahoo.com>
2019-09-28 15:17:31 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,27 +1,139 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ItemDetails from './item-details';
+import NewItem from './new-item';
+import EditItem from './edit-item';
+import ItemService from './shared/item-service';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.itemService = new ItemService();
+    this.onSelect = this.onSelect.bind(this);
+    this.onNewItem = this.onNewItem.bind(this);
+    this.onEditItem = this.onEditItem.bind(this);
+    this.onCancel = this.onCancel.bind(this);
+    this.onCancelEdit = this.onCancelEdit.bind(this);
+    this.onCreateItem = this.onCreateItem.bind(this);
+    this.onUpdateItem = this.onUpdateItem.bind(this);
+    this.onDeleteItem = this.onDeleteItem.bind(this);
+    this.state = {
+      showDetails: false,
+      editItem: false,
+      selectedItem: null,
+      newItem: null
+    }
+  }
+
+  componentDidMount() {
+      this.getItems();
+  }
+
   render() {
+    const items = this.state.items;
+    if(!items) return null;
+    const showDetails = this.state.showDetails;
+    const selectedItem = this.state.selectedItem;
+    const newItem = this.state.newItem;
+    const editItem = this.state.editItem;
+    const listItems = items.map((item) =>
+      <li key={item.link} onClick={() => this.onSelect(item.link)}>
+         <span className="item-name">{item.name}</span>&nbsp;|&nbsp; {item.summary}
+      </li>
+    );
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <ul className="items">
+            {listItems}
+          </ul>
+          <br/>
+          <button type="button" name="button" onClick={() => this.onNewItem()}>New Item</button>
+          <br/>
+            {newItem && <NewItem onSubmit={this.onCreateItem} onCancel={this.onCancel}/>}
+            {showDetails && selectedItem && <ItemDetails item={selectedItem} onEdit={this.onEditItem}  onDelete={this.onDeleteItem} />}
+            {editItem && selectedItem && <EditItem onSubmit={this.onUpdateItem} onCancel={this.onCancelEdit} item={selectedItem} />}
       </div>
     );
+  }
+
+  getItems() {
+    this.itemService.retrieveItems().then(items => {
+          this.setState({items: items});
+        }
+    );
+  }
+
+  onSelect(itemLink) {
+    this.clearState();
+    this.itemService.getItem(itemLink).then(item => {
+      this.setState({
+          showDetails: true,
+          selectedItem: item
+        });
+      }
+    );
+  }
+
+  onCancel() {
+    this.clearState();
+  }
+
+  onNewItem() {
+    this.clearState();
+    this.setState({
+      newItem: true
+    });
+  }
+
+  onEditItem() {
+    this.setState({
+      showDetails: false,
+      editItem: true,
+      newItem: null
+    });
+  }
+
+  onCancelEdit() {
+    this.setState({
+      showDetails: true,
+      editItem: false,
+      newItem: null
+    });
+  }
+
+  onUpdateItem(item) {
+    this.clearState();
+    this.itemService.updateItem(item).then(item => {
+        this.getItems();
+      }
+    );
+  }
+
+  onCreateItem(newItem) {
+    this.clearState();
+    this.itemService.createItem(newItem).then(item => {
+        this.getItems();
+      }
+    );
+  }
+
+  onDeleteItem(itemLink) {
+    this.clearState();
+    this.itemService.deleteItem(itemLink).then(res => {
+        this.getItems();
+      }
+    );
+  }
+
+  clearState() {
+    this.setState({
+      showDetails: false,
+      selectedItem: null,
+      editItem: false,
+      newItem: null
+    });
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-it('renders without crashing', () => {
+App.prototype.setState = jest.fn(function(updatedState) {
+  this.state = Object.assign(this.state, updatedState);
+});
+
+test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+  test('snapshot', () => {
+    const component = renderer.create(<App />);
+    const tree = component.toJSON();
+    console.log(tree);
+    expect(tree).toMatchSnapshot();
+  });
+  test('onNewItem', () => {
+    const component = new App();
+    expect(component.state).toEqual({
+        showDetails: false,
+        editItem: false,
+        selectedItem: null,
+        newItem: null
+      });
+    component.onNewItem();
+    expect(component.setState.mock.calls.length).toEqual(2);
+    expect(component.state).toEqual({
+      showDetails: false,
+      editItem: false,
+      selectedItem: null,
+      newItem: true
+    });
+  });
+  test('onEditItem', () => {
+  });
+  test('onCancelEdit', () => {
+  });
+  test('onCancel', () => {
+  });
+  test('onCreateItem', () => {
+  });
+  test('onUpdateItem', () => {
+  });
+  test('onDeleteItem', () => {
+  });
