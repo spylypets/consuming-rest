5c06b1befa4bda9775cfc9b6030255e03af26dea
spylypets <sergiy1@yahoo.com>
2019-09-28 10:03:55 +0200

    Add files via upload

---
+++ src/model/collection-item.js
@@ -0,0 +1,11 @@
+class CollectionItem {
+    constructor(
+        name: string,
+        summary: string,
+        year: number,
+        country: string,
+        description?: string,
+        selfLink?: string) {}
+}
+
+export default CollectionItem;
