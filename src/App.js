a053119759a51ef7dbd6378842f253fb0388aca2
spylypets <sergiy1@yahoo.com>
2019-10-02 15:48:24 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import ItemDetails from './item-details';
 import NewItem from './new-item';
 import EditItem from './edit-item';
-import ItemService from './shared/item-service';
+import ItemService from './shared/mock-item-service';
 
 class App extends Component {
 
